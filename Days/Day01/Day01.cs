ac02a9c8e13a8f3b86324433cc5f789623309740
Kamil Golec <kamil.golec@ceneo.pl>
2020-12-01 14:29:20 +0100

    reff

--- Days/Day01/Day01.cs
+++ Days/Day01/Day01.cs
@@ -26,7 +26,7 @@ namespace advent_of_code_2020.Days.Day01 {
                 .SelectMany((x, i) => report.Skip(i + 1)
                     .SelectMany((w, iw) => report.Skip(iw + 2), Tuple.Create), (x, y) => Tuple.Create(x, y.Item1, y.Item2))
                 .Where(t => t.Item1 + t.Item2 + t.Item3 == 2020)
-                .Select(t => t.Item1* t.Item2 * t.Item3)
+                .Select(t => t.Item1 * t.Item2 * t.Item3)
                 .First()
                 .ToString();
         }
