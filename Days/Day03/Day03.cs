cb5edd70c186564749836f7233084aa7551a7eaf
Kamil Golec <kumalgfilms@gmail.com>
2020-12-03 11:59:59 +0100

    refactor

--- Days/Day03/Day03.cs
+++ Days/Day03/Day03.cs
@@ -2,15 +2,15 @@
 
 namespace advent_of_code_2020.Days.Day03 {
     public class Day03 : Day {
-        private long TreesForSlope(int right, int down) => InputLines
-            .Where((line, i) => i % down == 0 && i != 0)
-            .Select((line, index) => line.ElementAt((index + 1) * right % line.Length))
+        private long TreesForSlope((int right, int down) slope) => InputLines
+            .Where((line, index) => index % slope.down == 0 && index != 0)
+            .Select((line, index) => line.ElementAt((index + 1) * slope.right % line.Length))
             .Count(c => c == '#');
 
-        public override string FirstStar() => TreesForSlope(3, 1).ToString();
+        public override string FirstStar() => TreesForSlope((3, 1)).ToString();
 
-        public override string SecondStar() => new[] { (1, 1), (3, 1), (5, 1), (7, 1), (1, 2) }
-            .Select(s => TreesForSlope(s.Item1, s.Item2))
+        public override string SecondStar() => new (int right, int down)[] { (1, 1), (3, 1), (5, 1), (7, 1), (1, 2) }
+            .Select(TreesForSlope)
             .Aggregate((a, b) => a * b)
             .ToString();
     }
