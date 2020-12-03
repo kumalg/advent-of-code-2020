d59404cb39167dd7eef2ced301a22f5228c11559
Kamil Golec <kumalgfilms@gmail.com>
2020-12-03 10:07:16 +0100

    expression body

--- Days/Day03/Day03.cs
+++ Days/Day03/Day03.cs
@@ -2,24 +2,18 @@
 
 namespace advent_of_code_2020.Days.Day03 {
     public class Day03 : Day {
-        private long TreesForSlope(int right, int down) {
-            return InputLines
-                .Where((line, i) => i % down == 0 && i != 0)
-                .Select((line, index) => string
-                    .Concat(Enumerable.Repeat(line, (index + 1) * right / line.Length + 1))
-                    .ElementAt((index + 1) * right))
-                .Count(c => c == '#');
-        }
+        private long TreesForSlope(int right, int down) => InputLines
+            .Where((line, i) => i % down == 0 && i != 0)
+            .Select((line, index) => string
+                .Concat(Enumerable.Repeat(line, (index + 1) * right / line.Length + 1))
+                .ElementAt((index + 1) * right))
+            .Count(c => c == '#');
 
-        public override string FirstStar() {
-            return TreesForSlope(3, 1).ToString();
-        }
+        public override string FirstStar() => TreesForSlope(3, 1).ToString();
 
-        public override string SecondStar() {
-            return new[] { (1, 1), (3, 1), (5, 1), (7, 1), (1, 2) }
-                .Select(s => TreesForSlope(s.Item1, s.Item2))
-                .Aggregate((a, b) => a * b)
-                .ToString();
-        }
+        public override string SecondStar() => new[] { (1, 1), (3, 1), (5, 1), (7, 1), (1, 2) }
+            .Select(s => TreesForSlope(s.Item1, s.Item2))
+            .Aggregate((a, b) => a * b)
+            .ToString();
     }
 }
