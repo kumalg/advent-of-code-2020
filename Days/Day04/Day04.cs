6b04cb8360b4d84a157637a478ca92687f22c55c
Kamil Golec <kumalgfilms@gmail.com>
2020-12-04 10:14:22 +0100

    sorted

--- Days/Day04/Day04.cs
+++ Days/Day04/Day04.cs
@@ -13,8 +13,8 @@ namespace advent_of_code_2020.Days.Day04 {
             "hgt" => pair.Value.EndsWith("cm") && int.Parse(pair.Value[0..^2]) is >= 150 and <= 193 ||
                      pair.Value.EndsWith("in") && int.Parse(pair.Value[0..^2]) is >= 59 and <= 76,
             "hcl" => new Regex(@"^#[a-f-0-9]{6}$").IsMatch(pair.Value),
-            "ecl" => eyeColors.Contains(pair.Value),
             "pid" => new Regex(@"^\d{9}$").IsMatch(pair.Value),
+            "ecl" => eyeColors.Contains(pair.Value),
             "cid" => true,
             _ => true,
         };
