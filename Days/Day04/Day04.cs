4c768530b052426f34883bd849806ef638ba9da8
Kamil Golec <kumalgfilms@gmail.com>
2020-12-04 10:03:00 +0100

    cleaning

--- Days/Day04/Day04.cs
+++ Days/Day04/Day04.cs
@@ -8,9 +8,8 @@ namespace advent_of_code_2020.Days.Day04 {
             "byr" => int.Parse(pair.Value) is >= 1920 and <= 2002,
             "iyr" => int.Parse(pair.Value) is >= 2010 and <= 2020,
             "eyr" => int.Parse(pair.Value) is >= 2020 and <= 2030,
-            "hgt" => pair.Value.EndsWith("cm")
-                        ? int.Parse(pair.Value[0..^2]) is >= 150 and <= 193
-                        : pair.Value.EndsWith("in") && int.Parse(pair.Value[0..^2]) is >= 59 and <= 76,
+            "hgt" => pair.Value.EndsWith("cm") && int.Parse(pair.Value[0..^2]) is >= 150 and <= 193 ||
+                     pair.Value.EndsWith("in") && int.Parse(pair.Value[0..^2]) is >= 59 and <= 76,
             "hcl" => new Regex(@"^#[a-f-0-9]{6}$").IsMatch(pair.Value),
             "ecl" => "amb|blu|brn|gry|grn|hzl|oth".Split("|").Contains(pair.Value),
             "pid" => new Regex(@"^\d{9}$").IsMatch(pair.Value),
