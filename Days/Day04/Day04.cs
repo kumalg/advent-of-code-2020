f02782bbd4c22f018df6d5a85dfe102a3740eff1
Kamil Golec <kumalgfilms@gmail.com>
2020-12-04 09:47:25 +0100

    day 4

---
+++ Days/Day04/Day04.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace advent_of_code_2020.Days.Day04 {
+    public class Day04 : Day<int> {
+        private static bool ValidateKey(KeyValuePair<string, string> pair) => pair.Key switch {
+            "byr" => int.Parse(pair.Value) is >= 1920 and <= 2002,
+            "iyr" => int.Parse(pair.Value) is >= 2010 and <= 2020,
+            "eyr" => int.Parse(pair.Value) is >= 2020 and <= 2030,
+            "hgt" => pair.Value.EndsWith("cm")
+                        ? int.Parse(pair.Value.Substring(0, pair.Value.Length - 2)) is >= 150 and <= 193
+                        : pair.Value.EndsWith("in")
+                            ? int.Parse(pair.Value.Substring(0, pair.Value.Length - 2)) is >= 59 and <= 76
+                            : false,
+            "hcl" => new Regex(@"^#[a-f-0-9]{6}$").IsMatch(pair.Value),
+            "ecl" => "amb|blu|brn|gry|grn|hzl|oth".Split("|").Contains(pair.Value),
+            "pid" => pair.Value.Length == 9 && new Regex(@"\d{9}").IsMatch(pair.Value),
+            "cid" => true,
+            _ => true,
+        };
+
+        private readonly string[] mandatoryFields = new[] { "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid" };
+
+        private IEnumerable<Dictionary<string, string>> Passports => Input
+            .Split("\n\n")
+            .Select(p => new Regex(@"[\s]")
+                .Split(p).Where(p => p != "")
+                .Select(kv => kv.Split(":"))
+                .ToDictionary(d => d[0], d => d[1]));
+
+        public override int FirstStar() => Passports
+            .Count(p => !mandatoryFields.Except(p.Select(l => l.Key)).Any());
+
+        public override int SecondStar() => Passports
+            .Where(p => !mandatoryFields.Except(p.Select(l => l.Key)).Any())
+            .Count(p => p.Select(ValidateKey).All(k => k));
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using advent_of_code_2020.Days;
-using advent_of_code_2020.Days.Day03;
+using advent_of_code_2020.Days.Day04;
 
 namespace advent_of_code_2020 {
     class Program {
-        static void Main(string[] args) => ShowDayResult(new Day03());
+        static void Main(string[] args) => ShowDayResult(new Day04());
 
         static void ShowDayResult(IDay day) {
             Console.WriteLine($"{ day.GetType().Name }");
