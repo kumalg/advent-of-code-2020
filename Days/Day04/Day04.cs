f7ee9843df65ffb709a8ccbd862464b75408300a
Kamil Golec <kumalgfilms@gmail.com>
2020-12-04 10:05:30 +0100

    statics

--- Days/Day04/Day04.cs
+++ Days/Day04/Day04.cs
@@ -4,6 +4,8 @@ using System.Text.RegularExpressions;
 
 namespace advent_of_code_2020.Days.Day04 {
     public class Day04 : Day<int> {
+        private static readonly string[] mandatoryFields = new[] { "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid" };
+        private static readonly string[] eyeColors = new[] { "amb", "blu", "brn", "gry", "grn", "hzl", "oth" };
         private static bool ValidateKey(KeyValuePair<string, string> pair) => pair.Key switch {
             "byr" => int.Parse(pair.Value) is >= 1920 and <= 2002,
             "iyr" => int.Parse(pair.Value) is >= 2010 and <= 2020,
@@ -11,13 +13,11 @@ namespace advent_of_code_2020.Days.Day04 {
             "hgt" => pair.Value.EndsWith("cm") && int.Parse(pair.Value[0..^2]) is >= 150 and <= 193 ||
                      pair.Value.EndsWith("in") && int.Parse(pair.Value[0..^2]) is >= 59 and <= 76,
             "hcl" => new Regex(@"^#[a-f-0-9]{6}$").IsMatch(pair.Value),
-            "ecl" => "amb|blu|brn|gry|grn|hzl|oth".Split("|").Contains(pair.Value),
+            "ecl" => eyeColors.Contains(pair.Value),
             "pid" => new Regex(@"^\d{9}$").IsMatch(pair.Value),
             "cid" => true,
             _ => true,
         };
-
-        private readonly string[] mandatoryFields = new[] { "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid" };
 
         private IEnumerable<Dictionary<string, string>> Passports => Input
             .Split("\n\n")
