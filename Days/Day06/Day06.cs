2a6ba6c33a6a18bf6f47fd895b0ba4fb7e829606
Kamil Golec <kumalgfilms@gmail.com>
2020-12-06 11:37:45 +0100

    Update Day06.cs

--- Days/Day06/Day06.cs
+++ Days/Day06/Day06.cs
@@ -5,19 +5,17 @@ namespace advent_of_code_2020.Days.Day06 {
 
         public override int FirstStar() => InputText
             .Split("\n\n")
-            .Select(l => l.Replace("\n", "")
+            .Sum(l => l.Replace("\n", "")
                 .ToCharArray()
                 .Distinct()
-                .Count())
-            .Sum();
+                .Count());
 
         public override int SecondStar() => InputText
             .Split("\n\n")
-            .Select(l => l
+            .Sum(l => l
                 .Split()
                 .Select(l => l.ToCharArray())
                 .Aggregate((a, b) => a.Intersect(b).ToArray())
-                .Length)
-            .Sum();
+                .Length);
     }
 }
