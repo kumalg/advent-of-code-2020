ae1589bc5fddaf1f8a001409960b9144e2c6854c
Kamil Golec <kumalgfilms@gmail.com>
2020-12-21 18:47:57 +0100

    Day 19 finished

--- Days/Day19/Day19.cs
+++ Days/Day19/Day19.cs
@@ -7,86 +7,102 @@ namespace advent_of_code_2020.Days {
     public class Day19 : Day<int> {
         private static readonly Regex RuleLineRegex = new Regex(@"(\d+): (.+)");
 
-        private static IList<string> Optionsy(int ruleId, string rule, Dictionary<int, string> rules, Dictionary<int, int> rulesCount, int maxMatch = int.MaxValue) {
-            if (ruleId == 8) {
-                rulesCount[8] = rulesCount[8] + 1;
-                if (rulesCount[8] > maxMatch) {
-                    return new List<string>() { "" };
-                }
-            }
-            if (ruleId == 11) {
-                rulesCount[11] = rulesCount[8] + 1;
-                if (rulesCount[11] > maxMatch) {
-                    return new List<string>() { "" };
-                }
-            }
+        private static (Dictionary<int, string> Rules, List<string> Messages) GetRulesAndMessages(string inputText) {
+            var sections = inputText
+                .Split("\n\n")
+                .ToList();
+
+            var rules = sections
+                .ElementAt(0)
+                .Split("\n")
+                .Select(line => RuleLineRegex.Match(line).Groups.Values)
+                .ToDictionary(
+                    v => int.Parse(v.ElementAt(1).Value),
+                    v => v.ElementAt(2).Value
+                );
+
+            var messages = sections
+                .ElementAt(1)
+                .Split("\n")
+                .ToList();
+
+            return (rules, messages);
+        }
+
+        private static IList<string> RuleOptions( string rule, Dictionary<int, string> rules) {
             if (rule.StartsWith("\"")) {
                 return new List<string>() { rule.Replace("\"", "") };
             }
+
             if (rule.Contains("|")) {
-                //var eightCount = eightCounty;
-                //var elevenCount = elevenCounty;
                 return rule
                     .Split(" | ")
-                    .Select(subrule => Optionsy(int.MaxValue, subrule, rules, rulesCount, maxMatch))
-                    .SelectMany(v => v)
+                    .SelectMany(subrule => RuleOptions(subrule, rules))
                     .Where(cc => !string.IsNullOrEmpty(cc))
                     .ToList();
             }
 
-            var jak = rule
+            return rule
                 .Split(" ")
                 .Select(int.Parse)
-                .Select(id => Optionsy(id, rules[id], rules, rulesCount, maxMatch))
-                .ToList();
-            return jak.Aggregate((sum, current) => sum.Where(ss => !string.IsNullOrEmpty(ss)).Distinct().Select(s => current.Where(cc => !string.IsNullOrEmpty(cc)).Distinct().Select(c => s + c).ToList()).SelectMany(v => v).ToList());
-            //return new[] { "a" }.ToList();
+                .Select(id => RuleOptions(rules[id], rules))
+                .ToList()
+                .Aggregate((sum, current) => sum
+                    .Where(ss => !string.IsNullOrEmpty(ss))
+                    .Distinct()
+                    .SelectMany(s => current
+                        .Where(cc => !string.IsNullOrEmpty(cc))
+                        .Distinct()
+                        .Select(c => s + c)
+                        .ToList()
+                    )
+                    .ToList()
+                );
         }
 
         public override int FirstStar() {
-            var sections = InputText
-                .Split("\n\n")
-                .ToList();
-            var stringyRules = sections
-                .ElementAt(0)
-                .Split("\n")
-                .Select(line => RuleLineRegex.Match(line).Groups.Values)
-                .ToDictionary(
-                    v => int.Parse(v.ElementAt(1).Value),
-                    v => v.ElementAt(2).Value
-                );
-            var rulesCount = stringyRules.ToDictionary(v => v.Key, v => 0);
-            rulesCount.Add(int.MaxValue, 0);
-            var rule0Options = Optionsy(0, stringyRules[0], stringyRules, rulesCount);
-            return sections
-                .ElementAt(1)
-                .Split("\n")
-                .Count(s => rule0Options.Contains(s));
+            var (rules, messages) = GetRulesAndMessages(InputText);
+
+            var rule0Options = RuleOptions(rules[0], rules);
+            return messages.Count(s => rule0Options.Contains(s));
         }
 
         public override int SecondStar() {
-            var sections = InputText
-                .Split("\n\n")
-                .ToList();
-            var stringyRules = sections
-                .ElementAt(0)
-                .Split("\n")
-                .Select(line => RuleLineRegex.Match(line).Groups.Values)
-                .ToDictionary(
-                    v => int.Parse(v.ElementAt(1).Value),
-                    v => v.ElementAt(2).Value
-                );
-            var rulesCount = stringyRules.ToDictionary(v => v.Key, v => 0);
-            rulesCount.Add(int.MaxValue, 0);
-            var messages = sections
-                .ElementAt(1)
-                .Split("\n");
-            stringyRules[8] = "42 | 42 8";
-            stringyRules[11] = "42 31 | 42 11 31";
-            var longestMessage = messages.OrderByDescending(m => m.Length).First().Length;
+            var (rules, messages) = GetRulesAndMessages(InputText);
 
-            var rule0Options = Optionsy(0, stringyRules[0], stringyRules, rulesCount, 10);
-            return messages.Count(s => rule0Options.Contains(s));
+            // 8: 42 | 42 8
+            // 11: 42 31 | 42 11 31
+
+            var rule42Options = RuleOptions(rules[42], rules);
+            var rule31Options = RuleOptions(rules[31], rules);
+
+            var county = 0;
+
+            foreach(var message in messages) {
+                var count42 = 0;
+                var count31 = 0;
+                var remainingMessage = message;
+
+                var is42match = rule42Options.FirstOrDefault(o => remainingMessage.StartsWith(o));
+                while (is42match != null) {
+                    remainingMessage = remainingMessage[is42match.Length..];
+                    is42match = rule42Options.FirstOrDefault(o => remainingMessage.StartsWith(o));
+                    count42++;
+                }
+
+                var is31match = rule31Options.FirstOrDefault(o => remainingMessage.EndsWith(o));
+                while (is31match != null) {
+                    remainingMessage = remainingMessage[..^is31match.Length];
+                    is31match = rule31Options.FirstOrDefault(o => remainingMessage.EndsWith(o));
+                    count31++;
+                }
+
+                if (remainingMessage == "" && count42 > 0 & count31 > 0 && count42 > count31) {
+                    county++;
+                }
+            }
+
+            return county;
         }
     }
 }
