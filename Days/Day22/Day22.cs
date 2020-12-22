ae3fb19449fae29ad5a3a0b74ca98b5dd8d1c0eb
Kamil Golec <kumalgfilms@gmail.com>
2020-12-22 10:39:38 +0100

    Update Day22.cs

--- Days/Day22/Day22.cs
+++ Days/Day22/Day22.cs
@@ -19,49 +19,52 @@ namespace advent_of_code_2020.Days {
 
         private static string RoundToString(Player[] players) => string.Join("", players.Select(p => $"({string.Join("|", p.Cards)})"));
 
-        private Player PlayGame(Player[] players, bool withSubGames) {
+        private (Player Winner, Player Looser) PlayGame(Player[] players, bool withSubGames) {
             var playedGames = new HashSet<string>();
 
             while (players.All(p => p.Cards.Count > 0)) {
                 if (!playedGames.Add(RoundToString(players))) {
-                    return players[0];
+                    return (players[0], players[1]);
                 }
 
-                var topCards = players.Select(p => (playerId: p.Id, card: p.Cards.First())).ToList();
+                (Player Winner, Player Looser) result;
+
+                var topCards = players.ToDictionary(p => p.Id, p => p.Cards.First());
 
                 if (withSubGames && players.All(p => p.Cards.Count > p.Cards.First())) {
-                    var subGameWinner = PlayGame(players.Select(p => new Player { Id = p.Id, Cards = p.Cards.Skip(1).Take(p.Cards.First()).ToList() }).ToArray(), withSubGames);
-
-                    foreach (var player in players) {
-                        player.Cards.RemoveAt(0);
-                    }
-
-                    players.ElementAt(subGameWinner.Id).Cards.AddRange(subGameWinner.Id > 0 ? topCards.Select(t => t.card).Reverse() : topCards.Select(t => t.card));
+                    result = PlayGame(players.Select(p => new Player {
+                        Id = p.Id,
+                        Cards = p.Cards.Skip(1).Take(topCards[p.Id]).ToList()
+                    }).ToArray(), withSubGames);
                 }
                 else {
-                    var orderedTopCards = topCards.OrderByDescending(v => v.card).ToList();
-                    foreach (var player in players) {
-                        player.Cards.RemoveAt(0);
-                    }
-
-                    players.ElementAt(orderedTopCards.First().playerId).Cards.AddRange(orderedTopCards.Select(t => t.card));
+                    var orderedPlayers = players.OrderByDescending(p => p.Cards.First()).ToArray();
+                    result = (orderedPlayers[0], orderedPlayers[1]);
                 }
+
+                foreach (var player in players) {
+                    player.Cards.RemoveAt(0);
+                }
+
+                players.First(p => p.Id == result.Winner.Id).Cards.AddRange(new[] { topCards[result.Winner.Id], topCards[result.Looser.Id] });
             }
-            return players
-                .Where(p => p.Cards.Count > 0)
-                .First();
+
+            var resultedOrder = players.OrderByDescending(p => p.Cards.Count).ToArray();
+            return (resultedOrder[0], resultedOrder[1]);
         }
 
         public override long FirstStar() => PlayGame(ToPlayers(InputText), withSubGames: false)
-                .Cards
-                .Reverse<int>()
-                .Select((v, id) => v * (id + 1))
-                .Sum();
+            .Winner
+            .Cards
+            .Reverse<int>()
+            .Select((v, id) => v * (id + 1))
+            .Sum();
 
         public override long SecondStar() => PlayGame(ToPlayers(InputText), withSubGames: true)
-                .Cards
-                .Reverse<int>()
-                .Select((v, id) => v * (id + 1))
-                .Sum();
+            .Winner
+            .Cards
+            .Reverse<int>()
+            .Select((v, id) => v * (id + 1))
+            .Sum();
     }
 }
